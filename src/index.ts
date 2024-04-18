446b25b0aba074f1e9713dad25beb370ae9eed5c
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2024-04-18 12:51:43 -0500

    Add support for DNSLink having an IPNS entry and using did:ipns:[NAME CID] for the ID instead.

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,23 @@ import { unixfs } from "@helia/unixfs";
 import { ipns } from "@helia/ipns";
 import { peerIdFromString } from "@libp2p/peer-id";
 import isFQDN from "validator/lib/isFQDN.js";
+import { dns, RecordType } from "@multiformats/dns";
 
 import { err } from "./utils";
 
+const DNSLINK_PREFIX = "dnslink=/ipns/";
+
 export function getResolver(helia: HeliaLibp2p) {
   const heliaFs = unixfs(helia),
-    heliaName = ipns(helia);
+    heliaName = ipns(helia),
+    resolver = dns();
 
   async function resolve(
     did: string,
     parsed: ParsedDID,
   ): Promise<DIDResolutionResult> {
     let cid: CIDType, didDocument: DIDDocumentType, finalCid: CIDType;
+    let didId = parsed.id;
     try {
       const queryParams = new Map();
       const didPath = parsed.path || ".well-known/did.json";
@@ -39,6 +44,18 @@ export function getResolver(helia: HeliaLibp2p) {
         cid = CID.parse(queryParams.get("versionId"));
       } else {
         if (isFQDN(parsed.id)) {
+          // If DNS has IPNS entry stored then use the IPNS CID as the DID ID instead.
+          const answer = (
+            await resolver.query(`_dnslink.${parsed.id}`, {
+              types: [RecordType.TXT],
+            })
+          ).Answer[0];
+          if (
+            answer.data.startsWith(DNSLINK_PREFIX) &&
+            answer.data.split("/").length === 3
+          ) {
+            didId = answer.data.substring(DNSLINK_PREFIX.length);
+          }
           cid = (await heliaName.resolveDNSLink(parsed.id)).cid;
         } else {
           const peer = peerIdFromString(parsed.id);
@@ -59,7 +76,7 @@ export function getResolver(helia: HeliaLibp2p) {
       return err("invalidDoc");
     }
 
-    if (didDocument.id !== `did:ipns:${parsed.id}`) {
+    if (didDocument.id !== `did:ipns:${didId}`) {
       return err("invalidDoc", "DID in document doesn't match");
     }
 

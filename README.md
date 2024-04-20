051f46c18701e1a64bd97539962030e8aa1b7193
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2024-04-20 12:28:58 -0500

    Change Package Name Support DNSLink and Forwarding DIDs

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # ipns-did-resolver
 
-**NOTE:** The [DID-IPNS spec](https://misterupkeep.github.io/did-ipns-spec/) is unstable.
-
 A resolver library for the IPNS method, to be used with the
-[`did-resolver`](https://www.npmjs.com/package/did-resolver) library.
+[`did-resolver`](https://www.npmjs.com/package/@did-ipns/resolver) library.
 
 IPNS method resolution works with both IPNS (Peer ID hash) CIDs and DNSLink
-domains. Paths will be resolved as UnixFS blocks.  
+domains. Paths will be resolved as UnixFS blocks.
 
 Note: The default DID path is `.well-known/did.json` which is similar to `did:web`.
 
@@ -17,7 +15,7 @@ it will throw an error.
 
 ```ts
 import { Resolver } from "did-resolver";
-import { getResolver } from "ipns-did-resolver";
+import { getResolver } from "@did-ipns/resolver";
 
 import { createHeliaHTTP } from '@helia/http'
 
--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,31 @@ import type {
   DIDResolutionResult,
 } from "did-resolver";
 import type { CID as CIDType } from "multiformats";
-
 import { DIDDocument } from "did-doc";
-
 import { CID } from "multiformats";
 import type { HeliaLibp2p } from "helia";
 import { unixfs } from "@helia/unixfs";
 import { ipns } from "@helia/ipns";
 import { peerIdFromString } from "@libp2p/peer-id";
 import isFQDN from "validator/lib/isFQDN.js";
-
+import { dns, RecordType } from "@multiformats/dns";
+import { parse } from "did-resolver";
 import { err } from "./utils";
+
+const DNSLINK_PREFIX = "dnslink=/ipns/";
 
 export function getResolver(helia: HeliaLibp2p) {
   const heliaFs = unixfs(helia),
-    heliaName = ipns(helia);
+    heliaName = ipns(helia),
+    resolver = dns();
 
   async function resolve(
     did: string,
     parsed: ParsedDID,
+    depth = 0,
   ): Promise<DIDResolutionResult> {
     let cid: CIDType, didDocument: DIDDocumentType, finalCid: CIDType;
+    let didId = parsed.id;
     try {
       const queryParams = new Map();
       const didPath = parsed.path || ".well-known/did.json";
@@ -39,6 +43,18 @@ export function getResolver(helia: HeliaLibp2p) {
         cid = CID.parse(queryParams.get("versionId"));
       } else {
         if (isFQDN(parsed.id)) {
+          // If DNS has IPNS entry stored then use the IPNS CID as the DID ID instead.
+          const answer = (
+            await resolver.query(`_dnslink.${parsed.id}`, {
+              types: [RecordType.TXT],
+            })
+          ).Answer[0];
+          if (
+            answer.data.startsWith(DNSLINK_PREFIX) &&
+            answer.data.split("/").length === 3
+          ) {
+            didId = answer.data.substring(DNSLINK_PREFIX.length);
+          }
           cid = (await heliaName.resolveDNSLink(parsed.id)).cid;
         } else {
           const peer = peerIdFromString(parsed.id);
@@ -59,8 +75,13 @@ export function getResolver(helia: HeliaLibp2p) {
       return err("invalidDoc");
     }
 
-    if (didDocument.id !== `did:ipns:${parsed.id}`) {
+    if (didDocument.id !== `did:ipns:${didId}`) {
       return err("invalidDoc", "DID in document doesn't match");
+    }
+
+    // Forward recursively until DID is resolved with a maximum depth of 10
+    if (typeof didDocument.forward === "string") {
+      return resolve(didDocument.forward, parse(didDocument.forward), depth++);
     }
 
     return {
